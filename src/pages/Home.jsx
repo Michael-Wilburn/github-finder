6e882fdc5c2199a2de58ef733b345b9c4f380554
Michael Wilburn <michael-mw@hotmail.com>
2023-04-25 23:21:11 -0300

    Revert "Adding custom enviroment variables"
    This reverts commit 31cb10666c23793ef6da232986b8ba9aecd51fa1.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ export default function Home() {
     return(
         <div>
             <h1 className="text-6xl">Welcome</h1>
-            {process.env.REACT_APP_GITHUB_TOKEN}
         </div>
     )
 }
