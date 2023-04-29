438995fdc58cd5963ec1e028134f6f9816470463
Michael Wilburn <michael-mw@hotmail.com>
2023-04-29 18:32:48 -0300

    User page component created

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import About from './pages/About';
 import NotFound from './pages/NotFound';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import User from './pages/User';
 import { GithubProvider } from './context/github/GithubContext';
 import { AlertProvider } from './context/alert/AlertContext';
-
 function App() {
   return (
     <GithubProvider>
@@ -20,6 +20,7 @@ function App() {
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
                 <Route path="/notfound" element={<NotFound />} />
                 <Route path="/*" element={<NotFound />} />
               </Routes>
