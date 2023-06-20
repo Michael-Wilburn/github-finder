729be4b7e4ede9af97835140c85b729b99e0350a
Michael Wilburn <michael-mw@hotmail.com>
2023-06-20 00:08:58 -0300

    RepoList and RepoItem created with it context

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,27 +1,53 @@
-import {FaCodepen, FaStore, FaUserFriends, FaUsers} from 'react-icons/fa'
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa'
 import { useEffect, useContext } from "react"
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Spinner from '../components/layout/Spinner'
+import RepoList from '../components/repos/RepoList'
 import GithubContext from "../context/github/GithubContext"
 import { useParams } from "react-router-dom"
 
 export default function User() {
-    const {getUser, user, loading} = useContext(GithubContext)
+  const { getUser, user, loading, getUserRepos, repos } = useContext(GithubContext)
 
-    const params = useParams()
+  const params = useParams()
 
-    useEffect(()=>{
-        getUser(params.login)
-    },[])
+  useEffect(() => {
+    getUser(params.login)
+    getUserRepos(params.login)
+  }, [])
 
-    const {name,type,avatar_url,location,bio,twitter_username,login,html_url,followers,following,public_repos,public_gists,hireable,} = user
+  const {
+    name,
+    type,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    twitter_username,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user
 
-    if(loading){
-        return <Spinner/>
-    }
+  if (loading) {
+    return <Spinner />
+  }
 
-    return <>
-       <div className='w-full mx-auto lg:w-10/12'>
+  // NOTE: check for valid url to users website
+
+  const websiteUrl = blog?.startsWith('http') ? blog : 'https://' + blog
+
+  // NOTE: if you are having problems with the name and login showing at the top
+  // of the image then you need the className='flex-grow-0' on the <p> tag
+  // default styling on <p> in daisyUI now has flex-grow-1
+
+  return (
+    <>
+      <div className='w-full mx-auto lg:w-10/12'>
         <div className='mb-4'>
           <Link to='/' className='btn btn-ghost'>
             Back To Search
@@ -40,31 +66,109 @@ export default function User() {
               </div>
             </div>
           </div>
-                <div className="col-span-2">
-                    <div className="mb-6">
-                    <h1 className='text-3xl card-title'>
-                        {name}
-                        <div className='ml-2 mr-1 badge badge-success'>{type}</div>
-                        {hireable && (
-                        <div className='mx-1 badge badge-info'>Hireable</div>
-                        )}
-                    </h1>
-                    <p>{bio}</p>
-                        <div className='mt-4 card-actions'>
-                        <a
-                            href={html_url}
-                            target='_blank'
-                            rel='noreferrer'
-                            className='btn btn-outline'
-                        >
-                            Visit Github Profile
-                        </a>
-                        </div>
-                    </div>
-                </div>
 
-
+          <div className='col-span-2'>
+            <div className='mb-6'>
+              <h1 className='text-3xl card-title'>
+                {name}
+                <div className='ml-2 mr-1 badge badge-success'>{type}</div>
+                {hireable && (
+                  <div className='mx-1 badge badge-info'>Hireable</div>
+                )}
+              </h1>
+              <p>{bio}</p>
+              <div className='mt-4 card-actions'>
+                <a
+                  href={html_url}
+                  target='_blank'
+                  rel='noreferrer'
+                  className='btn btn-outline'
+                >
+                  Visit Github Profile
+                </a>
+              </div>
             </div>
+
+            <div className='w-full rounded-lg shadow-md bg-base-100 stats'>
+              {location && (
+                <div className='stat'>
+                  <div className='stat-title text-md'>Location</div>
+                  <div className='text-lg stat-value'>{location}</div>
+                </div>
+              )}
+              {blog && (
+                <div className='stat'>
+                  <div className='stat-title text-md'>Website</div>
+                  <div className='text-lg stat-value'>
+                    <a href={websiteUrl} target='_blank' rel='noreferrer'>
+                      {websiteUrl}
+                    </a>
+                  </div>
+                </div>
+              )}
+              {twitter_username && (
+                <div className='stat'>
+                  <div className='stat-title text-md'>Twitter</div>
+                  <div className='text-lg stat-value'>
+                    <a
+                      href={`https://twitter.com/${twitter_username}`}
+                      target='_blank'
+                      rel='noreferrer'
+                    >
+                      {twitter_username}
+                    </a>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
-     </>
+
+        <div className='w-full py-5 mb-6 rounded-lg bg-base-100 stats'>
+          <div className='grid grid-cols-1 md:grid-cols-4'>
+            <div className='stat shadow-md'>
+              <div className='stat-figure text-secondary'>
+                <FaUsers className='text-3xl md:text-5xl' />
+              </div>
+              <div className='stat-title pr-5'>Followers</div>
+              <div className='stat-value pr-5 text-3xl md:text-4xl'>
+                {followers}
+              </div>
+            </div>
+
+            <div className='stat shadow-md'>
+              <div className='stat-figure text-secondary'>
+                <FaUserFriends className='text-3xl md:text-5xl' />
+              </div>
+              <div className='stat-title pr-5'>Following</div>
+              <div className='stat-value pr-5 text-3xl md:text-4xl'>
+                {following}
+              </div>
+            </div>
+
+            <div className='stat shadow-md'>
+              <div className='stat-figure text-secondary'>
+                <FaCodepen className='text-3xl md:text-5xl' />
+              </div>
+              <div className='stat-title pr-5'>Public Repos</div>
+              <div className='stat-value pr-5 text-3xl md:text-4xl'>
+                {public_repos}
+              </div>
+            </div>
+
+            <div className='stat shadow-md'>
+              <div className='stat-figure text-secondary'>
+                <FaStore className='text-3xl md:text-5xl' />
+              </div>
+              <div className='stat-title pr-5'>Public Gists</div>
+              <div className='stat-value pr-5 text-3xl md:text-4xl'>
+                {public_gists}
+              </div>
+            </div>
+          </div>
+        </div>
+        <RepoList repos={repos} />
+      </div>
+    </>
+  )
 }
