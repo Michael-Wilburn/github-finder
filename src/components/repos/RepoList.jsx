75387a43a5630491364b602c4e364cee935fa12f
Michael Wilburn <michael-mw@hotmail.com>
2023-06-20 00:50:47 -0300

    RepoItem and RepoList modifed

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -1,14 +1,16 @@
 import PropTypes from 'prop-types'
-
+import RepoItem from './RepoItem'
 
 function RepoList({ repos }) {
     return (
         <div className='rounded-lg shadow-lg card bg-base-100'>
-            <div className="card-body">
-                <h2 className="text-3xl my-4 font-bold card-title">
+            <div className='card-body'>
+                <h2 className='text-3xl my-4 font-bold card-title'>
                     Latest Repositories
                 </h2>
-                {repos.map((repo) => <h3>{repo.name}</h3>)}
+                {repos.map((repo) => (
+                    <RepoItem key={repo.id} repo={repo} />
+                ))}
             </div>
         </div>
     )
@@ -18,4 +20,4 @@ RepoList.propTypes = {
     repos: PropTypes.array.isRequired,
 }
 
-export default RepoList;
+export default RepoList
